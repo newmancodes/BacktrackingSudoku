872413869471b6c52d20e7e82c7ec53cd1ad4703
Stephen Newman <steve@newman.digital>
2021-11-11 00:40:35 +0000

    Add starter for ten tetris sum,

---
+++ Blazor.UI/Domain/AllCellsSumToTargetConstraint.cs
@@ -0,0 +1,20 @@
+﻿namespace Blazor.UI.Domain
+{
+    public class AllCellsSumToTargetConstraint : IConstraint
+    {
+        private readonly IReadOnlyCollection<Cell> _cells;
+        private readonly int _target;
+
+        public AllCellsSumToTargetConstraint(IEnumerable<Cell> cells, int target)
+        {
+            _cells = cells.ToList().AsReadOnly();
+            _target = target;
+        }
+
+        public bool IsValid()
+        {
+            return _cells.All(c => c.Value.HasValue)
+                && _cells.Sum(c => c.Value.Value) == _target;
+        }
+    }
+}
